c8fd140557864949119a999cb77ca09b73100acc
welvins <johnwelvins@gmail.com>
2016-10-17 22:28:01 -0200

    Update Temperatura Interna.ino

--- Arduino/Temperatura Interna.ino
+++ Arduino/Temperatura Interna.ino
@@ -1,78 +1,95 @@
-<pre>#include<SPI.h>
-const int SPIInitDelay = 200;    // SPI initialization delay
-const int SPIDelay = 20;         // SPI delay for reset/start pin
-const int ADResetPulse = 1;      // ADC reset pulse length in us defined by ADTreset()
+#include <SPI.h>
+
+const int ADResetPulse = 1;      // ADC reset (min. 4x244 = 976 ns, low), 244 ns = 1/4,096 MHz;
 const int ADResetDelay = 1;      // ADC delay afrter reset in ms min 0.6 ms
-long dataIN[3];
- 
-void setup()
+int mux1settings;
+int a =1;
+
+
+void setup() 
 {
- SPIinitialize(); // Arduino setup SPI
-// AD1247 wait 20ms, after power up, to start sending data. 2s delay in SPIinitialize
- ADreset(); // ADC reset before sending data.
- ADsetup();
  Serial.begin(115200);
+ delay (10);
+ SPIinitialize(); // Arduino setup SPI.
+ ADreset(); // ADC " hardware" reset before sending data.
+ ADsetup();
 }
- 
-void loop()
-{
- if (digitalRead(9) == 0){ADread();}    // read data when ADC dataReady is low
+
+
+void loop() 
+{ 
+   if (digitalRead(9) == 0)
+       {
+          ADread(); // read data when ADC dataReady is low
+       }
+
 }
  
 void ADread()
 {
  digitalWrite(10, LOW);
- SPI.transfer(0x12);
- for (int i = 0; i <= 2; i++){
- dataIN[i] = SPI.transfer(0xff);
- Serial.print(dataIN[i],HEX);
+ delayMicroseconds(1);        // atraso mínimo de 10 ns (tcssc).
+ SPI.transfer(0x12);          // Transfere a leitura do ADC para o buffer de saida
+ /* A estrutura abixo salva byte a byte do bauffer de saida do ADC, na mesma vairiavel (regData), e com o bit-shifting,
+ quando estamos salvando os bytes em um vertor, perdiamos o zero à esquerda o que complicava, na hora da converção para DEC */ 
+ int32_t regData;
+    regData |= SPI.transfer(0xff); 
+    regData <<= 8;
+    regData |= SPI.transfer(0xff); 
+    regData <<= 8;
+    regData |= SPI.transfer(0xff); 
+    Serial.print(regData,HEX);
+    Serial.print(" <--> ");
+    Serial.print(regData,DEC); 
+    Serial.print(" <--> ");
  }
- 
- digitalWrite(10, HIGH);
- Serial.println(&amp;quot;&amp;quot;);
- }
- 
+
 void ADsetup()
 {
- digitalWrite(10, LOW);
- SPI.transfer(0x42); // mux1 (write command)
- SPI.transfer(0x01); // sys0 (write command)
- SPI.transfer(0x30); // mux1 (data) - antes 33 agora 30
- SPI.transfer(0x00); // sys0 (data) - antes 10 agora 00
- SPI.transfer(0x40); // mux0 (write command)
- SPI.transfer(0x00); // não escreve nada
- SPI.transfer(0x01); // Ain1 Adc positive input; Ain0 negative inout
- SPI.transfer(0x16); // stop data conti (SDATAC command)
- delay(210); // delay to avoid read errors (em 24set16)
- digitalWrite(10, HIGH);
-// delay(210); // delay to avoid read errors (comentado em 24set16)
- }
- 
-void ADreset()
+  digitalWrite(10, LOW);       // habilita a comunicação SPI.
+  delayMicroseconds(1);        // atraso mínimo de 10 ns.
+  SPI.transfer(0x06);          // Reset de "software"
+  delay(ADResetDelay);         // atraso mínimo de 0,6 ms.
+  SPI.transfer(0x16);          // stop data continuous (SDATAC command); Comando tem efeito após concluído conversão do ADC.
+  delay(210);                  // Atraso mínimo de 1000(miliseconds)/taxa de amostragem (SPS), no nosso caso é 5.
+  
+  SPI.transfer(0x42);           // mux1      Seleciona o registro para escrita (write command)
+  SPI.transfer(0x01);           // sys0      (write command) - o registro sys0  é 0x43 (0x42 + 0x01)
+  SPI.transfer(0x33);           // mux1      Ativa a ref. interna (e IDAC) e seleciona VrefExt.
+  SPI.transfer(0x00);           // sys0      Ganho 1 e taxa de amostragem 5/s,
+// Na leitura da temperatura interna assume-se sempre ganho unitario, independente do ganho selecionado.
+  delayMicroseconds(10);        // atraso mínimo de 7x244 = 1708 ns (tsccs).
+  digitalWrite(10, HIGH);       // libera o barramento de comunicação SPI.
+  }
+
+  
+void ADreset()  // Reset de "hardware"
 {
  digitalWrite( 8, LOW);
- delay(ADResetDelay);
+ delay(ADResetPulse);
  digitalWrite( 8 , HIGH);
- delay(ADResetDelay);
+ delay(ADResetDelay); 
  }
+
  
 void SPIinitialize()
 {
  // Arduino ADC interface (wire accordingly)
  // inverse means that when pin is low relavent function is active e.g.:
  // DRDY low means ADC has data to send
- pinMode(10, OUTPUT); // pin 10 as CS (inverse)
- pinMode(9, INPUT); // pin 09 as DRDY (inverse)
- pinMode(8, OUTPUT); // pin 08 as reset (inverse)
- pinMode(7, OUTPUT); // pin 07 as start
+ pinMode(10, OUTPUT); // pin 10 as CS (inverse) - CS = Chip Select, habilita comunicação (em low) SPI com ADS1247.
+ pinMode(9, INPUT); // pin 09 as DRDY (inverse) - LOW indica conversão (ADC) completa.
+ pinMode(8, OUTPUT); // pin 08 as reset (inverse) - LOW (min. 4x244 ns =976 ns; 244 ns = 1/4,096 MHz) p/ reset.
+ pinMode(7, OUTPUT); // pin 07 as start - High (min. 3x244 = 732 ns) inicia conversão (ADC). POde ficar high sempre.
  //attachInterrupt(digitalPinToInterrupt(2) , ADread, LOW); // pin 09 as DRDY (inverse)
- digitalWrite(10, HIGH); // pins set to high
- digitalWrite(8 , HIGH);
- digitalWrite(7 , HIGH);
+ digitalWrite(10, HIGH); // pins set to high, desablita a comunicação SPI. 
+ digitalWrite(8 , HIGH); // operação normal, reset em LOW.
+ digitalWrite(7 , HIGH); // operação normal, ADC convertendo continuamente.
  
  // Arduino SPI settings (change first line according to IC specifications)
  // defines SPI comunication type and initializes module
  SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1)); // SPI setup SPI communication
- delay(SPIInitDelay); // SPI delay for initialization
+ delay(200); // SPI delay for initialization
  SPI.begin(); // SPI Module 'wake up'
+ 
 }
